eb07a0aea48612ef8a8b55111cc2cc3bdc60c199
Mauricio Barva <mauriciobarva@gmail.com>
2021-11-15 01:52:29 -0500

    formulario terminado

--- src/app/user/pages/profile/profile.component.ts
+++ src/app/user/pages/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-profile',
@@ -9,13 +10,31 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ProfileComponent implements OnInit {
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
   formProfile: FormGroup = this.fb.group({
-    name: [, [Validators.required]],
-    email: [, [Validators.required, Validators.pattern(this.emailPattern)]]
+    username: [, [Validators.required]],
+    email: [, [Validators.required, Validators.pattern(this.emailPattern)]],
+    genre: ['M'],
+    biography: []
   });
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  requiredError(name: string) {
+    return this.formProfile.get(name)?.hasError('required') && this.formProfile.get(name)?.touched;
+  }
+
+  requiredPattern(name: string) {
+    return this.formProfile.get(name)?.hasError('pattern') && this.formProfile.get(name)?.touched;
+  }
+
+  saveForm() {
+    if (this.formProfile.invalid) {
+      this.formProfile.markAllAsTouched();
+      return;
+    }
+    Swal.fire('Todo correcto', 'El formulario es válido', 'success');
   }
 
 }
