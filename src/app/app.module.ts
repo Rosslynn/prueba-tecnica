f3cb7e64f6ecff9b5e48aebf80fa0f51be94f9c4
Mauricio Barva <mauriciobarva@gmail.com>
2021-11-14 21:45:33 -0500

    modulo shared añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedComponent } from './shared/shared.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
