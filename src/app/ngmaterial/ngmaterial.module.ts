68445bfe68000a2a80f12de561995a07874e5e53
Mauricio Barva <mauriciobarva@gmail.com>
2021-11-15 01:22:12 -0500

    formulario inciiado

--- src/app/ngmaterial/ngmaterial.module.ts
+++ src/app/ngmaterial/ngmaterial.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
 
 
 
 @NgModule({
   imports: [
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRadioModule
   ],
   exports: [
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRadioModule
   ]
 })
 export class NgmaterialModule { }
--- src/app/user/pages/profile/profile.component.ts
+++ src/app/user/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+  formProfile: FormGroup = this.fb.group({
+    name: [, [Validators.required]],
+    email: [, [Validators.required, Validators.pattern(this.emailPattern)]]
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
